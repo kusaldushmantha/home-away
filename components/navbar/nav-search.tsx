5dd20c8ca0c53a47ef4c51060f3a1d6a7842e3de
kusalkankanamge <17594647+kusaldushmantha@users.noreply.github.com>
2025-07-06 12:01:15 +0800

    add nav bar search and logo

--- components/navbar/nav-search.tsx
+++ components/navbar/nav-search.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Input } from "@/components/ui/input";
 
 const NavSearch = () => {
-    return (
-        <div>NavSearch</div>
-    )
+  return (
+    <Input type="text" placeholder="Search a property" className="max-w-xs dark:bg-muted"/>
+  )
 }
 
 export default NavSearch
